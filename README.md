e77df4713801e5d64e37c87ca017bcb784e4950c
Thinh Phan <vincent.phan@firstidea.vn>
2019-04-30 14:35:54 +0700

    added fetching 30 days cached

---
+++ README.md
@@ -0,0 +1,19 @@
+# Try Block Data in SQLite3
+
+## Installation
+
+Using `conda`:
+
+`conda install --yes --file requirements.txt`
+
+Using `pip`
+
+`pip install -r requirements.txt`
+
+OR convenience commands from `Hoffmann`:
+[Install only available packages using “conda install --yes --file requirements.txt” without error](https://stackoverflow.com/a/38609653/741197)
+
+`while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt`
+
+## Usage
+
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import sqlite3
 import json
 import time
 import os.path
+import random
+
 import requests as rq
+
 
 DB_NAME = "test.db"
 TABLE_NAME = "data"
@@ -38,24 +41,31 @@ def create_or_open_db(filename):
 
 db = create_or_open_db(DB_NAME)
 # Get a cursor object
-cursor = db.cursor()
-cursor.execute('SELECT SQLITE_VERSION()')
-print('version:', cursor.fetchone())
+_cursor = db.cursor()
+_cursor.execute('SELECT SQLITE_VERSION()')
+print('version:', _cursor.fetchone())
 
 
 # Create table
-def create_table(cursor):
+def create_tables(cursor=_cursor):
 	sql = '''CREATE TABLE IF NOT EXISTS {} 
-			(id INTEGER PRIMARY KEY, block INTEGER, day INTEGER, year INTEGER, scheduleId TEXT, slideId TEXT, updatedTime INTEGER);'''.format(TABLE_NAME)
+			(id INTEGER PRIMARY KEY, 
+			block INTEGER, day INTEGER, 
+			year INTEGER, 
+			scheduleId TEXT, 
+			slideId TEXT, 
+			updatedTime INTEGER);
+			'''.format(TABLE_NAME)
 	cursor.execute(sql)
 	print("Created table succesfully")
 
-create_table(cursor)
+create_tables()
 
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print(cursor.fetchall())
+'''Check again to make sure tables exist'''
+_cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(_cursor.fetchall())
 
-# Drop
+# Drop Table
 def drop_table(cursor, tablename):
 	sql = '''DROP TABLE {0}'''.format(tablename)
 	cursor.execute(sql)
@@ -63,64 +73,89 @@ def drop_table(cursor, tablename):
 # drop_table(cursor, "data")
 
 # Insert Data
-def insert_data(cursor):
+def insert_data(data, cursor =_cursor):
 	start_time = time.time()
-	cursor.executemany("INSERT INTO data (block, day, year, scheduleId, slideId, updatedTime) VALUES (:block,:day,:year,:scheduleId,:slideId,:updatedTime);", datastore)
+	cursor.executemany('''INSERT INTO data (block, day, year, scheduleId, slideId, updatedTime) 
+						VALUES (:block,:day,:year,:scheduleId,:slideId,:updatedTime);''', data)
 	db.commit()
 	print("insert_data --- %s seconds ---" % (time.time() - start_time))
+
+# insert_data(datastore)
 
 def print_table(cursor, tablename):
 	start_time = time.time()
 	print("\nEntire database contents:\n")
 	cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 	print(cursor.fetchall())
-	# for row in cursor.execute("SELECT * FROM {0};".format(tablename)):
-	cursor.execute("SELECT * FROM {0};".format(tablename))
-	print(len(cursor.fetchall()))
+
+	# cursor.execute("SELECT * FROM {};".format(tablename))
+	# print(len(cursor.fetchall()))
+
+	'''Faster than len(cursor.fetchall())'''
+	print(cursor.execute("SELECT COUNT(*) FROM {}".format(tablename)).fetchone()[0])
 	print("--- %s seconds ---" % (time.time() - start_time))
-	
-# print_table(cursor, "data")
 
-print(cursor.execute("SELECT COUNT(*) FROM data").fetchone()[0])
-# print('Last row id: %d' % cursor.lastrowid)
-
-
-cur_request_day = 1
-interval_request_day = 3
 
 # Fetch data every 5m to test network and fetch
-def fetch_data():
+def fetch_data(dates):
 	start_time = time.time()
 	print("\nFetch Schedule Blocks At Date \n")
 
 	url = 'https://neuraldisplay-admin-api.herokuapp.com/test/blocks'
 
-	querystring = {'days': [111, 112]}
+	querystring = {'days': dates}
 
 	res = rq.get(url, params=querystring)
 	print(res.url)
 	print(res.status_code)
 	
 	res_json = res.json()
+	# print(res_json)
 	print("fetch_data --- %s seconds ---" % (time.time() - start_time))
 	return res_json
 
-fetch_data()
+# fetch_data([1])
 
-# Save to DB
-# Retreive from DB
-# Repeat
+'''
+- Fetch datas
+- Save to DB
+- Repeat 30 days or request cocurrence 30 days
+- Retreive from DB
+'''
+CACHED_DATES = 30
+
+# cur_request_day = 1
+interval_request_day = 2
+
+start_time = time.time()
+for i in range(1, CACHED_DATES, interval_request_day):
+	req_range = range(i, i + interval_request_day)
+	print('\nRequet date: {}'.format(list(req_range)))
+	insert_data(fetch_data(req_range))
+
+print("Recurrence request --- %s seconds ---" % (time.time() - start_time))
+
+
+
 
 def get_schedule_by_date(cursor, date):
 	start_time = time.time()
-	print("\nGet Schedule Blocks At Date: {}\n".format(date))
-	sql = '''SELECT * FROM data WHERE day={};'''.format(date)
-	cursor.execute(sql)
-	print(len(cursor.fetchall()))
+	print("\nGet Schedule Blocks At Date: {}".format(date))
+	# sql = '''SELECT * FROM data WHERE day={};'''.format(date)
+	# cursor.execute(sql)
+	# print(len(cursor.fetchall()))
+
+	sql = "SELECT COUNT(*) FROM data WHERE day={};".format(date)
+	print(cursor.execute(sql).fetchone()[0])
+
 	print("get_schedule --- %s seconds ---" % (time.time() - start_time))
 
-get_schedule_by_date(cursor, 112)
+get_schedule_by_date(_cursor, random.randint(1, CACHED_DATES))
+
+
+print('\n------- Check and print all metrics again -------')
+print_table(_cursor, "data")
 
 # Done
-cursor.close()
+_cursor.close()
 db.close()
